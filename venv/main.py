af997228b07996527be9b29d739d8fc827295b60
Rafael <biehler.rafael@gmail.com>
2019-07-02 22:39:15 +0200

    main: Es wird jetzt ein Graph angezeigt der so in einer Endlosschleife aktualisert wird. Momentan ist noch kein Spektrum zu sehen, sondern nur die Wellenform.

--- venv/main.py
+++ venv/main.py
@@ -1,32 +1,38 @@
 import pyaudio  # Dieses Modul lässt uns auf das Aufnahmegerät zugreifen.
 import struct  # Dieses Modul wird benötigt, um die Binärdaten des Audiostreams in Integer umzuwandeln
+import numpy as np  # numpy
+import matplotlib.pyplot as plt
+import sys
 from scipy.fftpack import fft  # fft = fast fourier transform
 
 
 def main():
-    # Init
-    CHUNK = 1024  # Wieviele Samples in einen stream gelesen werden.
-    SAMPLE_RATE = 44100  # Die Abtastrate des Aufnahmegeräts, meist 44,1 kHz.
-    FORMAT = pyaudio.paInt16  # Die Zahlen sollen als Int16 ausgelesen werden.
-    CHANNELS = 1  # Unser Mikrofon nimmt in diesem Fall nur in Mono auf.
-
+    chunk = 4096
+    audio_format = pyaudio.paInt16
+    channels = 1
+    rate = 44100
     p = pyaudio.PyAudio()
-    print(p.get_default_input_device_info())  # .get("name") für nur Name
+    p.input_device_index = 7
+    stream = p.open(format=audio_format, channels=channels, rate=rate, input=True, output=True, frames_per_buffer=chunk)
 
-    stream = p.open(format=FORMAT, channels=CHANNELS, rate=SAMPLE_RATE, input=True, frames_per_buffer=CHUNK)
-    read_stream = stream.read(num_frames=CHUNK)
-    int_data = struct.unpack(str(2 * CHUNK) + 'B', read_stream)  # Konvertiert Binärdaten zu Integern (Werte von 0-255)
-    int_data_half = int_data[
-                    ::2]  # Jeden zweiten Wert nehmen, da jeder zweiter Wert der Audiowelle wieder zu x=0 zurückkehrt.
+    x = np.arange(0, 2 * chunk, 2)
+    plt.ion()
+    fig = plt.figure()
+    ax = fig.add_subplot()
+    ax.set_ylim(0, 255)
+    ax.set_xlim(0, chunk)
+    line, = ax.plot(x, np.random.rand(chunk))
 
-    # fft_data ist ein numpy ndarray der Länge CHUNK. Jedes Item der stellt eine komplexe Zahl dar.
-    # fft_data ist also unser Spektrum, an dem wir auslesen können, wie laut jede Frequenz gespielt wird.
-    # Diese Daten können wir benutzen, um unseren Music Visualizer anzusteuern.
-    fft_data = fft(int_data_half)
-
-    print(len(fft_data), type(fft_data), type(fft_data[0]), fft_data)
-    for array_item in fft_data:
-        print("Realteil: " + str(array_item.real) + " Imaginärteil: " + str(array_item.imag))
+    try:
+        while True:
+            data = stream.read(chunk)
+            data_int = np.array(struct.unpack(str(2 * chunk) + 'B', data), dtype='b')[::2] + 128
+            line.set_ydata(data_int)
+            fig.canvas.draw()
+            fig.canvas.flush_events()
+    except KeyboardInterrupt:
+        print("Das Beenden funktioniert irgendwie noch nicht....")
+        sys.exit(0)
 
 
 if __name__ == "__main__":
